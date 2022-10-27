65c6773de492a9988bedc92db1a3e839a69ebf20
adiyu <adiyudha323@gmail.com>
2022-10-27 13:36:17 +0700

    FISRT COMMIT

---
+++ routes/web.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+// Route::group(['prefix'=>'mahasiswa'], function(){
+//     Route::get('/pendaftaran', function () {
+//     	return 'Halaman Pendaftaran';
+// 	});
+//     Route::get('/ujian', function () {
+//     	return 'Halaman Ujian';
+// 	});
+//     Route::get('/nilai', function () {
+//     	return 'Halaman Nilai';
+// 	});
+// });
